cc62966a8902a6f687d91247f374c9649eaff952
Sandeep <sandeepkbh47@gmail.com>
2024-12-01 22:26:13 +0530

    ms_f_commit_2

--- controllers/ewayController.js
+++ controllers/ewayController.js
@@ -3,7 +3,7 @@ const logger = require('./../logger')
 exports.extendEwayBill = catchAsync(async (req, res, next) => {
     setTimeout(() => {
         logger.info("Done" + new Date().toTimeString())
-        res.status(200).json({ statusCode: 200 })
+        res.status(200).json({ statusCode: 200, msg: "Done" + new Date().toTimeString() })
     }, 1000)
     //return next(new Error('No token. Please login again'))
 })
